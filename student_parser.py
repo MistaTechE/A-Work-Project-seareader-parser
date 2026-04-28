7f4c353983ca1febb344c469d1eb31c7bcc4d9d9
Evan Meserve <emeser3@wgu.edu>
2026-04-28 21:43:23 +0000

    Got rid of try block because it was slowing it down student_parser.py

--- student_parser.py
+++ student_parser.py
@@ -47,31 +47,28 @@ def main():
         print("Make sure the PDF is saved in the same folder and named correctly.\n")
         sys.exit(1)
 
-    try: 
-        with pdfplumber.open(PDF_NAME) as pdf:
-            full_text = ""
-            for page in pdf.pages:
-                page_text = page.extract_text()
-                if page_text:
-                    full_text += page_text + " "
 
-        cleaned = clean_text(full_text)
-        df = extract_students(cleaned)
+    with pdfplumber.open(PDF_NAME) as pdf:
+        full_text = ""
+        for page in pdf.pages:
+            page_text = page.extract_text()
+            if page_text:
+                full_text += page_text + " "
 
-        # Filtering logic
-        df_30_plus = df[df["Actual Points"] >= 30.0]
-        df_10_to_29 = df[(df["Actual Points"] >= 10.0) & (df["Actual Points"] < 30.0)]
+    cleaned = clean_text(full_text)
+    df = extract_students(cleaned)
 
-        df_30_plus.to_csv("students_30_plus.csv", index=False)
-        df_10_to_29.to_csv("students_10_to_29_999.csv", index=False)
+    # Filtering logic
+    df_30_plus = df[df["Actual Points"] >= 30.0]
+    df_10_to_29 = df[(df["Actual Points"] >= 10.0) & (df["Actual Points"] < 30.0)]
 
-        messagebox.showinfo(
-            "Success",
-            "CSV files created successfully!"
-        )
+    df_30_plus.to_csv("students_30_plus.csv", index=False)
+    df_10_to_29.to_csv("students_10_to_29_999.csv", index=False)
 
-    except Exception as e:
-        messagebox.showerror("Error", str(e))
+    messagebox.showinfo(
+        "Success",
+        "CSV files created successfully!"
+    )
 
 # GUI Window
 root = tk.Tk()
